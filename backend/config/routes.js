f90632aeb64d36c100b106eeca35b9769231cf89
xLima12 <felipelima1910@gmail.com>
2025-02-11 18:40:41 -0300

    feat: add middlewares and routes

---
+++ backend/config/routes.js
@@ -0,0 +1,26 @@
+module.exports = (app) => {
+  app.route("/users").post(app.api.user.save).get(app.api.user.get);
+
+  app.route("/users/:id").put(app.api.user.save).get(app.api.user.getById);
+
+  app
+    .route("/categories")
+    .post(app.api.category.save)
+    .get(app.api.category.get);
+
+  app.route("/categories/tree").get(app.api.category.getTree);
+
+  app
+    .route("/categories/:id")
+    .put(app.api.category.save)
+    .get(app.api.category.getById)
+    .delete(app.api.category.remove);
+
+  app.route("/articles").post(app.api.article.save).get(app.api.article.get);
+
+  app
+    .route("/articles/:id")
+    .put(app.api.article.save)
+    .get(app.api.article.getById)
+    .delete(app.api.article.remove);
+};
