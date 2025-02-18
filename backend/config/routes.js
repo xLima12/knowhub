92a32c2ff12e091664a2c0f5e4a29f5783447ccc
xLima12 <felipelima1910@gmail.com>
2025-02-18 00:12:56 -0300

    feat: add route for articles

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -23,4 +23,6 @@ module.exports = (app) => {
     .put(app.api.article.save)
     .get(app.api.article.getById)
     .delete(app.api.article.remove);
+
+  app.route("/categories/:id/articles").get(app.api.article.getByCategory);
 };
