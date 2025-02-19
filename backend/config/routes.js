923f68ec339ec8631e4a2de1531b30eee1dc1f52
xLima12 <felipelima1910@gmail.com>
2025-02-18 23:12:36 -0300

    feat: add middleware in route for permission to access

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,28 +1,54 @@
+const admin = require("./admin");
 module.exports = (app) => {
-  app.route("/users").post(app.api.user.save).get(app.api.user.get);
+  app.post("/signup", app.api.user.save);
+  app.post("/signin", app.api.auth.signin);
+  app.post("validateToken", app.api.auth.validateToken);
 
-  app.route("/users/:id").put(app.api.user.save).get(app.api.user.getById);
+  app
+    .route("/users")
+    .all(app.config.passport.authenticate())
+    .post(admin(app.api.user.save))
+    .get(admin(app.api.user.get));
+
+  app
+    .route("/users/:id")
+    .all(app.config.passport.authenticate())
+    .put(admin(app.api.user.save))
+    .get(admin(app.api.user.getById));
 
   app
     .route("/categories")
-    .post(app.api.category.save)
-    .get(app.api.category.get);
+    .all(app.config.passport.authenticate())
+    .post(admin(app.api.category.save))
+    .get(admin(app.api.category.get));
 
-  app.route("/categories/tree").get(app.api.category.getTree);
+  app
+    .route("/categories/tree")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getTree);
 
   app
     .route("/categories/:id")
-    .put(app.api.category.save)
+    .all(app.config.passport.authenticate())
+    .put(admin(app.api.category.save))
     .get(app.api.category.getById)
-    .delete(app.api.category.remove);
+    .delete(admin(app.api.category.remove));
 
-  app.route("/articles").post(app.api.article.save).get(app.api.article.get);
+  app
+    .route("/articles")
+    .all(app.config.passport.authenticate())
+    .post(admin(app.api.article.save))
+    .get(admin(app.api.article.get));
 
   app
     .route("/articles/:id")
-    .put(app.api.article.save)
+    .all(app.config.passport.authenticate())
+    .put(admin(app.api.article.save))
     .get(app.api.article.getById)
-    .delete(app.api.article.remove);
+    .delete(admin(app.api.article.remove));
 
-  app.route("/categories/:id/articles").get(app.api.article.getByCategory);
+  app
+    .route("/categories/:id/articles")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getByCategory);
 };
