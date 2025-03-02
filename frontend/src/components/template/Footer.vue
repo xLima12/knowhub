d9341c73f6ceb1d65d172128a99f456f11827722
xLima12 <felipelima1910@gmail.com>
2025-03-02 15:51:15 -0300

    feat: add components

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <span
+      >Copyright <strong>Code Noir</strong> &copy; 2024-{{ currentYear }}</span
+    >
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background-color: #ddd;
+  color: #333;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 10px;
+}
+</style>
