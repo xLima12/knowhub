f7595688674d030815d7a2b80580055f112853cf
xLima12 <felipelima1910@gmail.com>
2025-03-02 15:55:17 -0300

    feat: add `AdminPages.vue`

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="admin-pages">
+    <PageTitle
+      icon="fa fa-cogs"
+      main="Administração do Sistema"
+      sub="Cadastro & Cia"
+    />
+    <div class="admin-pages-tabs">
+      <BCard no-body>
+        <BTabs card>
+          <BTab title="Artigos" active>
+            <ArticleAdmin />
+          </BTab>
+          <BTab title="Categorias">
+            <CategoryAdmin />
+          </BTab>
+          <BTab title="Usuários">
+            <UserAdmin />
+          </BTab>
+        </BTabs>
+      </BCard>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageTitle from "../template/PageTitle";
+import ArticleAdmin from "./ArticlesAdmin";
+import CategoryAdmin from "./CategoryAdmin";
+import UserAdmin from "./UserAdmin";
+
+export default {
+  name: "AdminPages",
+  components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin },
+};
+</script>
+
+<style></style>
