3c2a9af7e7f96d235cb18ff09f63007ff580f3f1
xLima12 <felipelima1910@gmail.com>
2025-03-03 19:16:58 -0300

    feat: add QuillEditor in project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,11 +7,11 @@ import App from "./App.vue";
 import store from "./config/store";
 import router from "./config/router";
 import axios from "axios";
-
-axios.defaults.headers.common.Authorization =
-  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkZlbGlwZSBMaW1hIiwiZW1haWwiOiJmbGltYUBjb2Rlbm9pci5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzQwODc4NDYxLCJleHAiOjE3NDExMzc2NjF9.lfV5mhNRMpAWPcthfnklcpyeZTDPScAOX3MO_4Zlc2k";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 const app = createApp(App);
+app.component("QuillEditor", QuillEditor);
 app.use(createBootstrap());
 app.use(store);
 app.use(router);
