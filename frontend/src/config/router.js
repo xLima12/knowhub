d853e3575f3a3e62505f691e35125f62ab4e73cc
xLima12 <felipelima1910@gmail.com>
2025-03-03 19:19:17 -0300

    refactor: move paste `assets` for paste `src` and add more 2 routes

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/components/home/Home";
 import AdminPages from "@/components/admin/AdminPages";
+import ArticlesByCategory from "@/components/article/ArticlesByCategory";
+import ArticleById from "@/components/article/ArticleById.vue";
 
 const routes = [
   {
@@ -13,6 +15,16 @@ const routes = [
     path: "/admin",
     name: "Admin",
     component: AdminPages,
+  },
+  {
+    name: "articlesByCategory",
+    path: "/categories/:id/articles",
+    component: ArticlesByCategory,
+  },
+  {
+    name: "articleById",
+    path: "/articles/:id",
+    component: ArticleById,
   },
 ];
 
