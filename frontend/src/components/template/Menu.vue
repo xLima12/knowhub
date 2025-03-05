88626f56d8413a301af42cfde0c8ce340ed8fe33
xLima12 <felipelima1910@gmail.com>
2025-03-04 22:36:29 -0300

    refactor: add navigation tree in the menu

--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -1,19 +1,73 @@
 <template>
-  <aside class="menu" v-show="isMenuVisible"></aside>
+  <aside class="menu" v-show="isMenuVisible">
+    <Tree
+      :value="treeData"
+      class="w-full md:w-[30rem]"
+      :filter="treeFilter"
+      selectionMode="single"
+      @node-select="onNodeSelect"
+      ref="tree"
+    />
+  </aside>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import Tree from "primevue/tree";
+import { baseApiUrl } from "@/global";
+import axios from "axios";
 export default {
   name: "Menu",
+  components: { Tree },
   computed: mapState(["isMenuVisible"]),
+  data: function () {
+    return {
+      treeFilter: "",
+      treeData: [],
+      props: {
+        key: "id",
+        label: "name",
+        children: "children",
+        filter: {
+          emptyText: "Categoria não encontrada",
+        },
+      },
+    };
+  },
+  methods: {
+    async getTreeData() {
+      const url = `${baseApiUrl}/categories/tree`;
+      const res = await axios.get(url);
+      this.treeData = res.data.map((item) => this.formatTreeNode(item));
+      console.log(this.treeData);
+    },
+    formatTreeNode(item) {
+      return {
+        key: item.id,
+        label: item.name,
+        to: `/categories/${item.id}/articles`,
+        children:
+          item.children && item.children.length > 0
+            ? item.children.map((child) => this.formatTreeNode(child))
+            : [],
+      };
+    },
+    onNodeSelect(node) {
+      if (node.to) {
+        this.$router.push(node.to);
+      }
+    },
+  },
+  mounted() {
+    this.getTreeData();
+  },
 };
 </script>
 
 <style>
 .menu {
   grid-area: menu;
-  background: linear-gradient(to right, #232526, #414345);
+  background: var(--p-tree-background);
 
   display: flex;
   flex-direction: column;
