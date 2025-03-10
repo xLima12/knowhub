a0d56ff890b5942eccad0a76d68b0f9bacda020a
xLima12 <felipelima1910@gmail.com>
2025-03-10 20:20:26 -0300

    refactor: name routes, and remove user mock

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -4,33 +4,51 @@ import Home from "@/components/home/Home";
 import AdminPages from "@/components/admin/AdminPages";
 import ArticlesByCategory from "@/components/article/ArticlesByCategory";
 import ArticleById from "@/components/article/ArticleById.vue";
+import Auth from "@/components/auth/Auth";
+import { userKey } from "@/global";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
     path: "/admin",
-    name: "Admin",
+    name: "adminPages",
     component: AdminPages,
+    meta: { requiresAdmin: true },
   },
   {
-    name: "articlesByCategory",
     path: "/categories/:id/articles",
+    name: "articlesByCategory",
     component: ArticlesByCategory,
   },
   {
-    name: "articleById",
     path: "/articles/:id",
+    name: "articleById",
     component: ArticleById,
+  },
+  {
+    path: "/auth",
+    name: "auth",
+    component: Auth,
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const json = localStorage.getItem(userKey);
+  if (to.matched.some((record) => record.meta.requiresAdmin)) {
+    const user = JSON.parse(json);
+    user && user.admin ? next() : next({ path: "/" });
+  } else {
+    next();
+  }
 });
 
 export default router;
--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -1,12 +1,10 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    isMenuVisible: true,
-    user: {
-      name: "Usuário Mock",
-      email: "mock@codenoir.com.br",
-    },
+    isMenuVisible: false,
+    user: null,
   },
   getters: {
     getUser(state) {
@@ -15,10 +13,25 @@ export default createStore({
   },
   mutations: {
     toggleMenu(state, isVisible) {
+      if (!state.user) {
+        state.isMenuVisible = false;
+        return;
+      }
+
       if (isVisible === undefined) {
         state.isMenuVisible = !state.isMenuVisible;
       } else {
         state.isMenuVisible = isVisible;
+      }
+    },
+    setUser(state, user) {
+      state.user = user;
+      if (user) {
+        axios.defaults.headers.common["Authorization"] = `bearer ${user.token}`;
+        state.isMenuVisible = true;
+      } else {
+        delete axios.defaults.headers.common["Authorization"];
+        state.isMenuVisible = false;
       }
     },
   },
