0d38aef739ead73a20baece3f099f0028ae46e5d
xLima12 <felipelima1910@gmail.com>
2025-03-10 20:23:34 -0300

    feat: add component `Loading.vue`

---
+++ frontend/src/components/template/Loading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loading">
+    <ProgressSpinner
+      style="width: 50px; height: 50px"
+      strokeWidth="5"
+      fill="transparent"
+      animationDuration="2s"
+      aria-label="Loading"
+    />
+  </div>
+</template>
+
+<script>
+import ProgressSpinner from "primevue/progressspinner";
+
+export default {
+  name: "Loading",
+  components: { ProgressSpinner },
+};
+</script>
+
+<style>
+.loading {
+  grid-area: content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
