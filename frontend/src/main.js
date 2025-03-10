9ce92925a37f3e8a27e8296cd36447dc057b0022
xLima12 <felipelima1910@gmail.com>
2025-03-10 20:19:05 -0300

    feat: add Vue3Mq

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,14 +6,12 @@ import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 import App from "./App.vue";
 import store from "./config/store";
 import router from "./config/router";
-import axios from "axios";
+import "./config/axios";
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
-
-axios.defaults.headers.common.Authorization =
-  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkZlbGlwZSBMaW1hIiwiZW1haWwiOiJmbGltYUBjb2Rlbm9pci5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzQxMTM3NzE4LCJleHAiOjE3NDEzOTY5MTh9.fk05SBgeGqxjoOIpcGW74vOb3vcLfHbzDlwEUw64W3s";
+import { Vue3Mq } from "vue3-mq";
 
 const app = createApp(App);
 app.use(PrimeVue, {
@@ -24,6 +22,15 @@ app.use(PrimeVue, {
       darkModeSelector: "system",
       cssLayer: false,
     },
+  },
+});
+app.use(Vue3Mq, {
+  breakpoints: {
+    xs: 576,
+    sm: 768,
+    md: 960,
+    lg: 1140,
+    xl: Infinity,
   },
 });
 app.component("QuillEditor", QuillEditor);
